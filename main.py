19190a36287ffff9b5df9e4e8b04a2b7123dabfa
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2022-02-28 13:42:04 -0800

    update game()
    - worked on the game itself 
        - correct word correct place
        - correct word wrong place 
        - 5 tries

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#if letter is capital: correct letter, correct place
+#if letter is X: correct letter, correct place
 
-#if letter is lowercase: letter not found in word
+#if letter is 'x': correct letter, wrong place 
 
 import json
 import random
@@ -14,25 +14,44 @@ def fiveLetterWords():
         word = word.lower()
         if len(word) == 5:
             fiveLetters.append(word)      
-            sum +=1    
-    print(sum) 
     return fiveLetters
 
 def chooseWord(possibleWords):
-    word = possibleWords[random.randint(0, 3552)]
+    word = possibleWords[random.randint(0, 6370)]
     return word
     
 
+def game(answer):
+    print(answer)
+    sum = 0
+    for i in range(0,5):
+        print("instructions:\n -capital letter means correct letter at correct place \n -characters in single quotation marks mean correct letter at the wrong place \n -lowercase letters mean wrong letters at the wrong place")
+        guess = input("please enter a five letter word: ")
+        for i in range (0, 5):
+            sum += 1
+            if answer.lower() == guess.lower():
+                print("YAY you got this")
+                break
+            if sum == 5: 
+                if answer.lower() != guess.lower():
+                    print("OH NO its okay!")
+            guess = list(guess)
+            answer = list(answer)
+            #correct word, correct place
+            if answer[i]== guess[i]:
+                guess[i] = guess[i].upper()
+            #correct word, wrong place
+            elif guess[i] in answer:
+                str(guess[i])
+                guess[i] = "'" + guess[i] + "'"
+            
+        print(*guess, sep = " ")
 
-#def game():
-   # guess = input("please enter a five letter word")
-    #for letter in guess: 
-        
-
+    
 
 possibleWords = fiveLetterWords()
 word = chooseWord(possibleWords)
-
+game(word)
 
 
 
